127960b449154f0f3c393ae7bc11a39e62627a77
jay <dj.min43@gmail.com>
2022-05-07 19:28:31 +0900

    first java

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-V
-&com/example/helloworld/HelloWorld.java,2/f/2ff2594abbc58bde3b4d560f588c04dca34eeb4d
--- first-java/src/HelloWorld.java
+++ first-java/src/HelloWorld.java
@@ -1,7 +1,5 @@
-package com.example.helloworld;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hello world");
     }
 }
