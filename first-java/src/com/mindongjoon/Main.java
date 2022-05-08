1d01dd9d84de8d32e08b619b966461bea64d225d
jay <dj.min43@gmail.com>
2022-05-08 18:40:34 +0900

    add sanner

--- first-java/src/com/mindongjoon/Main.java
+++ first-java/src/com/mindongjoon/Main.java
@@ -1,12 +1,13 @@
 package com.mindongjoon;
 
-import java.text.FieldPosition;
-import java.text.NumberFormat;
-import java.text.ParsePosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-        String result = NumberFormat.getPercentInstance().format(0.3);
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("you are " + name);
     }
 }
