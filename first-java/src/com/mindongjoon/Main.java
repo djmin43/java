77789a2439a2268756eaa0ba0c27fcbfda0efa4a
jay <dj.min43@gmail.com>
2022-05-08 16:41:28 +0900

    add double quote

--- first-java/src/com/mindongjoon/Main.java
+++ first-java/src/com/mindongjoon/Main.java
@@ -2,7 +2,7 @@ package com.mindongjoon;
 
 public class Main {
     public static void main (String[] args) {
-        String message = "Hello Java";
-        System.out.println(message.indexOf("e"));
+        String message = "Hello \"Java\"";
+        System.out.println(message);
     }
 }
