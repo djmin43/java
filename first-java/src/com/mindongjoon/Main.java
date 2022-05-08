82888156379176a03ae8d72fe6176334202850f1
jay <dj.min43@gmail.com>
2022-05-08 17:00:04 +0900

    nested array

--- first-java/src/com/mindongjoon/Main.java
+++ first-java/src/com/mindongjoon/Main.java
@@ -4,10 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main (String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-
-        System.out.println(Arrays.toString(numbers));
+        int[][] numbers = { { 1, 2, 3}, { 4, 5, 6}};
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
