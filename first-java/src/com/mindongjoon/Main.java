10460d165bf0f08a2c418227fe07b42aaa7e479c
jay <dj.min43@gmail.com>
2022-05-08 16:17:54 +0900

    hello java

--- first-java/src/com/mindongjoon/Main.java
+++ first-java/src/com/mindongjoon/Main.java
@@ -2,7 +2,7 @@ package com.mindongjoon;
 
 public class Main {
     public static void main (String[] args) {
-        String message = "Hello World" + "!!";
+        String message = "Hello Java";
         System.out.println(message.indexOf("e"));
     }
 }
