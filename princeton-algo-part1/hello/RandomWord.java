29a9cbe404682345a3a58fe242fa354a4f989b8f
james <james.min@freedgrouptech.com>
2022-05-10 07:34:07 +0900

    give up!

--- princeton-algo-part1/hello/RandomWord.java
+++ princeton-algo-part1/hello/RandomWord.java
@@ -1,8 +1,8 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main (String[] args) {
-        StdOut.print("hihi");
     }
 }
