7463adc267861bcc3a3e4a8baf71d90d74e785fa
Jay Min <dj.min43@gmail.com>
2022-07-13 09:11:33 +0900

    bank transaction analyzer simple

--- bank-statement-machine/src/main/bank/BankTransactionAnalyzerSimple.java
+++
@@ -1,9 +0,0 @@
-package main.bank;
-public class BankTransactionAnalyzerSimple {
-    public static final String RESOURCES = "src/main/resources/";
-
-    public static void hello() {
-        System.out.println(" world");
-    }
-
-}
