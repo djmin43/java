2b45b56e42ee94bc683f6e323af549ae403eb874
jay <dj.min43@gmail.com>
2022-07-14 21:05:16 +0900

    get months

--- bank-statement-machine/src/main/bank/simple/BankTransactionAnalyzerSimple.java
+++ bank-statement-machine/src/main/bank/simple/BankTransactionAnalyzerSimple.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -13,8 +15,10 @@ public class BankTransactionAnalyzerSimple {
        final Path path = Paths.get(RESOURCES + args[0]);
        final List<String> lines = Files.readAllLines(path);
        double total = 0d;
+       final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-M-yyyy");
        for (final String line: lines) {
            final String[] columns = line.split(",");
+           final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
            final double amount = Double.parseDouble(columns[1]);
            total += amount;
        }
