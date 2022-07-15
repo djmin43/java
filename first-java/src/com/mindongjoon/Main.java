3705fccd13788a82c2ea4f3ec5ddd09652c7ad95
jay <dj.min43@gmail.com>
2022-07-16 07:59:38 +0900

    clean

--- first-java/src/com/mindongjoon/Main.java
+++ first-java/src/com/mindongjoon/Main.java
@@ -5,16 +5,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Employee jay = new Employee("jay");
-        jay.setAge(500);
-        jay.setSalary(100);
-        jay.setYearsOfExperience(100);
-        jay.getSalary();
-        jay.doubleSalary();
-        jay.getSalary();
-        jay.doubleSalary();
-        jay.getSalary();
-        jay.getYearsOrExperience();
-        jay.getAge();
     }
 }
