02a1fff04d4ad3ac0672c2604dacb42382ac3d92
jay <dj.min43@gmail.com>
2022-07-15 21:14:28 +0900

    creating an object

--- first-java/src/com/mindongjoon/Animal.java
+++ first-java/src/com/mindongjoon/Animal.java
@@ -2,4 +2,8 @@ package com.mindongjoon;
 
 public class Animal {
     static int numberOfEars = 2;
+
+    public void growl() {
+       System.out.println("hey! I have" + numberOfEars + "ears!");
+    }
 }
--- first-java/src/com/mindongjoon/Main.java
+++ first-java/src/com/mindongjoon/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println(Animal.numberOfEars);
+        Animal dog = new Animal();
+        dog.growl();
     }
 }
