1da5eb3b8b9d1bf1483dd67a17f4069b45139443
jay <dj.min43@gmail.com>
2022-07-15 19:09:36 +0900

    with jan

--- bank-statement-machine/src/main/bank/simple/BankTransactionAnalyzerSimple.java
+++ bank-statement-machine/src/main/bank/simple/BankTransactionAnalyzerSimple.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -19,8 +20,10 @@ public class BankTransactionAnalyzerSimple {
        for (final String line: lines) {
            final String[] columns = line.split(",");
            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-           final double amount = Double.parseDouble(columns[1]);
-           total += amount;
+           if (date.getMonth() == Month.JANUARY) {
+               final double amount = Double.parseDouble(columns[1]);
+               total += amount;
+           }
        }
 
        System.out.println("The total for all transactions is " + total);
