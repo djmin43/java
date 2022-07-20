ecfe7b4ee9b97eed8242743722e95a39a2d82dfa
Jay Min <dj.min43@gmail.com>
2022-07-21 07:44:29 +0900

    banas-tutorial: class and oop

---
+++ banas-java-tutorial/src/com/java/OOPClass/Monster.java
@@ -0,0 +1,15 @@
+package com.java.OOPClass;
+
+public class Monster {
+
+    public final String TOMBSTONE = "Here Lies a Dead monster";
+
+    private int health = 500;
+    private int attack = 20;
+    private int movement = 2;
+    private int xPosition = 0;
+    private int yPosition = 0;
+    private boolean alive = true;
+    
+
+}
