6c631ce80f15d75566bfd727cfa7eac0b2bb6cb6
jay <dj.min43@gmail.com>
2022-07-23 10:49:03 +0900

    default constructor

--- banas-java-tutorial/src/com/java/OOPClass/Monster.java
+++ banas-java-tutorial/src/com/java/OOPClass/Monster.java
@@ -3,8 +3,8 @@ package com.java.OOPClass;
 public class Monster {
 
     public final String TOMBSTONE = "Here Lies a Dead monster";
-    private final int attack = 20;
-    private final int movement = 2;
+    private int attack = 20;
+    private int movement = 2;
     private final int xPosition = 0;
     private final int yPosition = 0;
     public String name = "Big Monster";
@@ -37,5 +37,23 @@ public class Monster {
             alive = false;
         }
     }
+
+    public Monster(int newHealth, int newAttack, int newMovement) {
+        this.health = newHealth;
+        this.attack = newAttack;
+        this.movement = newMovement;
+    }
+
+    //  Default Constructor
+    public Monster() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+
+
 
 }
