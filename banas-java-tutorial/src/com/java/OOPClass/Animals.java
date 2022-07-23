e76f65da3d74066f9014815fa9d54277d06f606d
jay <dj.min43@gmail.com>
2022-07-24 08:58:05 +0900

    cats

--- banas-java-tutorial/src/com/java/OOPClass/Animals.java
+++ banas-java-tutorial/src/com/java/OOPClass/Animals.java
@@ -2,33 +2,32 @@ package com.java.OOPClass;
 
 public class Animals {
     private String name = "Animal";
-    public String favFood = "Food";
+    public static String favFood = "Food";
 
     protected final void changeName(String newName) {
         this.name = newName;
     }
 
-    protected final String getName(){
+    protected final String getName() {
         return this.name;
     }
 
-    public void eatStuff(){
+    public void eatStuff() {
         System.out.println("Yum" + favFood);
     }
 
-    public void walkAround(){
+    public void walkAround() {
         System.out.println(this.name + "walks around");
     }
 
-    public Animals(){
+    public Animals() {
 
     }
 
-    public Animals(String name, String favFood){
+    public Animals(String name, String favFood) {
         this.changeName(name);
         this.favFood = favFood;
     }
-
 
 
 }
