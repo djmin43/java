6f11d1d8b7d02ad21d310eacf00fef2935740346
jay <dj.min43@gmail.com>
2022-07-24 18:17:29 +0900

    create an interface

---
+++ banas-java-tutorial/src/com/java/OOPClass/Drivable.java
@@ -0,0 +1,11 @@
+package com.java.OOPClass;
+
+public interface Drivable {
+
+    double PI = 3.14;
+    int getWheel();
+    void setWheels(int numWheels);
+    double getSpeed();
+    void setSpeed(double speed);
+
+}
