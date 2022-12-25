4bdd889e8cb729f745a8ba71e7f4dd3196824a6a
jay <dj.min43@gmail.com>
2022-12-25 20:01:42 +0900

    running tests

---
+++ study/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,42 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result1 = repository.findByName("spring1").get();
+        assertThat(result1).isEqualTo(member1);
+
+
+        Member result2 = repository.findByName("spring2").get();
+        assertThat(result2).isEqualTo(member2);
+
+    }
+
+}
