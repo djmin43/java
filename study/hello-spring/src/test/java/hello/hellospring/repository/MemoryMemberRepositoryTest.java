184baed952b5520005d8afc9973996c9743ffd61
jay <dj.min43@gmail.com>
2022-12-25 20:12:08 +0900

    writing more test with foreach

--- study/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ study/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,13 +1,21 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
 
     @Test
     public void save() {
@@ -33,10 +41,28 @@ class MemoryMemberRepositoryTest {
         Member result1 = repository.findByName("spring1").get();
         assertThat(result1).isEqualTo(member1);
 
-
         Member result2 = repository.findByName("spring2").get();
         assertThat(result2).isEqualTo(member2);
 
     }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+
+
+    }
+
+
 
 }
