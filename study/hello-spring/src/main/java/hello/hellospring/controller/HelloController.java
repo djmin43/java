189950c12e676a7f541a5437e6be9f53078be8ca
jay <dj.min43@gmail.com>
2022-12-25 17:56:05 +0900

    repo

--- study/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ study/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,14 +3,41 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hell(Model model) {
+    public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello";
-
     };
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+       return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
