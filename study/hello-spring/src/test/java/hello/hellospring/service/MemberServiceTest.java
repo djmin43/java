a945b12a38e1092b11cf0f9b7dd850a93a309b86
jay <dj.min43@gmail.com>
2022-12-26 20:37:27 +0900

    more test

--- study/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ study/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,15 +1,25 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
 
+    @AfterEach
+    public void afterEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
 
     @Test
     void join() {
@@ -25,6 +35,23 @@ class MemberServiceTest {
         assertThat(member.getName()).isEqualTo(findMember.getName());
     }
 
+    @Test
+    public void validateDuplicate() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        assertThrows(IllegalAccessError.class, () -> memberService.join(member2));
+
+        // then
+
+
+    }
     @Test
     void findMembers() {
     }
