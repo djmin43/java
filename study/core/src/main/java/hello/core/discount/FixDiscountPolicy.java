ce84bb9596b719fd5f44b6fbcbdfcfbde1237fab
jay <dj.min43@gmail.com>
2023-01-01 19:23:02 +0900

    get discount

---
+++ study/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discountFixAmount = 1000; // 1,000원 할인
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        }
+        return 0;
+    }
+}
--- study/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ study/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,15 +1,24 @@
 package hello.core.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
+    MemberService memberService = new MemberServiceImpl();
+
     @Test
     void join() {
         // given
+        Member member = new Member(1L, "test-1", Grade.VIP);
 
         // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(member.getId());
 
         // then
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+
     }
 }
