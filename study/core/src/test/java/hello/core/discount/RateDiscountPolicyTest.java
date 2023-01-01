f4f36368e5261b7af8cfa41324e2a923e6baaa1c
jay <dj.min43@gmail.com>
2023-01-01 20:32:40 +0900

    write and test rate discount policy

---
+++ study/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,28 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = rateDiscountPolicy.discount(member, 10000);
+
+        // then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+}
