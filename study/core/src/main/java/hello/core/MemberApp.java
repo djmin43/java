7823f1af7af155f8eeb739520a081490865de334
jay <dj.min43@gmail.com>
2023-01-01 11:42:21 +0900

    test given when then

---
+++ study/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("memberA = " + memberA.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
---
+++ study/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    @Test
+    void join() {
+        // given
+
+        // when
+
+        // then
+    }
+}
