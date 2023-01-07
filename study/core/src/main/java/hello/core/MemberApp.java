90ce314b6322ee4711ed4b159be2c791ab581ed7
jay <dj.min43@gmail.com>
2023-01-07 16:51:05 +0900

    dip

--- study/core/src/main/java/hello/core/MemberApp.java
+++ study/core/src/main/java/hello/core/MemberApp.java
@@ -7,7 +7,8 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
