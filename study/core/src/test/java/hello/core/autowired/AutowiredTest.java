af04b6e6110a69e716d1b01035142be39d2b1ac6
jay <dj.min43@gmail.com>
2023-01-20 20:26:16 +0900

    comment out autowired test

--- study/core/src/test/java/hello/core/autowired/AutowiredTest.java
+++ study/core/src/test/java/hello/core/autowired/AutowiredTest.java
@@ -11,25 +11,25 @@ import org.springframework.lang.Nullable;
 import java.util.Optional;
 
 public class AutowiredTest {
-
-    @Test
-    void AutowiredOption() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
-    }
-
-    static class TestBean {
-
-        @Autowired(required = false)
-        public void setNoBean1(Member noBean1) {
-            System.out.println("noBean1 = " + noBean1);
-        }
-        @Autowired(required = false)
-        public void setNoBean2(@Nullable Member noBean2) {
-            System.out.println("noBean1 = " + noBean2);
-        }
-        @Autowired
-        public void setNoBean3(Optional<Member> noBean3) {
-            System.out.println("noBean1 = " + noBean3);
-        }
-    }
+//
+//    @Test
+//    void AutowiredOption() {
+//        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+//    }
+//
+//    static class TestBean {
+//
+//        @Autowired(required = false)
+//        public void setNoBean1(Member noBean1) {
+//            System.out.println("noBean1 = " + noBean1);
+//        }
+//        @Autowired(required = false)
+//        public void setNoBean2(@Nullable Member noBean2) {
+//            System.out.println("noBean1 = " + noBean2);
+//        }
+//        @Autowired
+//        public void setNoBean3(Optional<Member> noBean3) {
+//            System.out.println("noBean1 = " + noBean3);
+//        }
+//    }
 }
