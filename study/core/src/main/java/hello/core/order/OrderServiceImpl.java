da3ed380d0800c0c22fc3bbedb3c8f0ab27d464b
jay <dj.min43@gmail.com>
2023-01-21 17:54:11 +0900

    qualifier

--- study/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ study/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,7 +15,7 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
