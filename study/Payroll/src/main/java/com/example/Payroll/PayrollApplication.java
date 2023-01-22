5e9e49e8afd495fb385fded357a683dfc8ff873c
jay <dj.min43@gmail.com>
2023-01-22 09:16:34 +0900

    payroll generate

---
+++ study/Payroll/src/main/java/com/example/Payroll/PayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.Payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollApplication.class, args);
+	}
+
+}
---
+++ study/Payroll/src/test/java/com/example/Payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
