02976c1343ff03ee47ef78daae782c825acbeb59
jay <dj.min43@gmail.com>
2023-01-23 08:58:03 +0900

    jpabook

---
+++ study/jpabook/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.8"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "jpagroup"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ study/jpabook/src/main/java/jpagroup/jpabook/JpabookApplication.java
@@ -0,0 +1,13 @@
+package jpagroup.jpabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpabookApplication.class, args);
+	}
+
+}
