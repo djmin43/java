c7e86e7887acf7fb80675468036b1ab7f47d21d3
jay <dj.min43@gmail.com>
2023-01-23 09:18:08 +0900

    test out lombok

---
+++ study/jpashop/src/main/java/jpagroup/jpashop/Hello.java
@@ -0,0 +1,12 @@
+package jpagroup.jpashop;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+
+    private String data;
+}
---
+++ study/jpashop/src/test/java/jpagroup/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpagroup.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
