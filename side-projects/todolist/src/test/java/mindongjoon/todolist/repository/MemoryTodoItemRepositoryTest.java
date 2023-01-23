417f325114801d6c007a80359202b39eb66d96c6
jay <dj.min43@gmail.com>
2023-01-23 15:52:02 +0900

    write delete test

--- side-projects/todolist/src/test/java/mindongjoon/todolist/repository/MemoryTodoItemRepositoryTest.java
+++ side-projects/todolist/src/test/java/mindongjoon/todolist/repository/MemoryTodoItemRepositoryTest.java
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,24 +21,33 @@ class MemoryTodoItemRepositoryTest {
 
     @Test
     void create() {
-        TodoItem todoItem = new TodoItem();
-        todoItem.setName("itemA");
-        todoItem.setDescription("this is item A");
-        repository.create(todoItem);
-        TodoItem result = repository.findById(todoItem.getId()).get();
+        TodoItem result = this.createNewTodoItem("itemA", "this is item A");
         assertThat(result.getName()).isEqualTo("itemA");
     }
 
     @Test
     void update() {
+        TodoItem result = this.createNewTodoItem("itemA", "this is item A");
+        result.setName("itemB");
+        repository.update(result.getId(), result);
+        TodoItem updatedItem = repository.findById(result.getId()).get();
+        assertThat(updatedItem.getName()).isEqualTo("itemB");
     }
 
     @Test
     void delete() {
+        TodoItem result = this.createNewTodoItem("itemA", "this will be deleted");
+        List<TodoItem> allTodoItems = repository.getAllTodoItems();
+        int addedSize = allTodoItems.size();
+        assertThat(addedSize).isEqualTo(1);
+        repository.delete(result.getId());
+        List<TodoItem> newTodoItems = repository.getAllTodoItems();
+        assertThat(newTodoItems.size()).isEqualTo(0);
     }
 
     @Test
     void findById() {
+
     }
 
     @Test
@@ -45,5 +56,13 @@ class MemoryTodoItemRepositoryTest {
 
     @Test
     void getAllTodoItems() {
+    }
+
+    public TodoItem createNewTodoItem(String name, String description) {
+        TodoItem todoItem = new TodoItem();
+        todoItem.setName(name);
+        todoItem.setDescription(description);
+        repository.create(todoItem);
+        return repository.findById(todoItem.getId()).get();
     }
 }
