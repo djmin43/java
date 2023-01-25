8cd7fe387f6c2a5d79a62a4e3ae29b53b7f2a5b5
jay <dj.min43@gmail.com>
2023-01-25 19:54:58 +0900

    revive jpa basic

---
+++ jpa-basic/src/main/java/com/example/jpabasic/JpaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpabasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBasicApplication.class, args);
+	}
+
+}
---
+++ jpa-basic/src/test/java/com/example/jpabasic/JpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
