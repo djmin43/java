112160080d7c46c6a74264b8255fb17fbc8dfe94
jay <dj.min43@gmail.com>
2023-01-25 21:28:12 +0900

    .

--- jpa-basic/src/test/java/com/example/jpabasic/repository/SomeRepositoryTest.java
+++ jpa-basic/src/test/java/com/example/jpabasic/repository/SomeRepositoryTest.java
@@ -1,23 +1,25 @@
-package com.example.jpabasic;
+package com.example.jpabasic.repository;
 
+import com.example.jpabasic.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class JpaMainTest {
+class SomeRepositoryTest {
+
+    @Autowired
+    SomeRepository someRepository;
 
     @Test
-    @Transactional
     void someTest() {
-        JpaMain jpaMain = new JpaMain();
-        jpaMain.someTest();
-
+        Member member = new Member();
+        someRepository.someTest(member);
     }
 
 }
