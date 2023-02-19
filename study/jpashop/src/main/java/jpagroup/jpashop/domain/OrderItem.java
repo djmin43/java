49cd72708b25c0107a5c8743f818637bafc9b4e0
jay <dj.min43@gmail.com>
2023-02-19 11:26:34 +0900

    ~ to one to lazy

--- study/jpashop/src/main/java/jpagroup/jpashop/domain/OrderItem.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/domain/OrderItem.java
@@ -17,11 +17,11 @@ public class OrderItem {
     private Long id;
 
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
