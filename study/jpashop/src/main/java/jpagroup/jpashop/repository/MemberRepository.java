67bb6d471f2fe30902259ad5f818a0c132d7b852
jay <dj.min43@gmail.com>
2023-02-19 18:32:46 +0900

    done member service

--- study/jpashop/src/main/java/jpagroup/jpashop/repository/MemberRepository.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/repository/MemberRepository.java
@@ -1,17 +1,17 @@
 package jpagroup.jpashop.repository;
 
 import jpagroup.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
