9149aa45d95ee810279a02df621f1a872f2269bf
jay <dj.min43@gmail.com>
2023-02-19 15:55:12 +0900

    bi-directional methods

--- study/jpashop/src/main/java/jpagroup/jpashop/domain/Category.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/domain/Category.java
@@ -34,4 +34,10 @@ public class Category {
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
+    // 연관관계 메소드
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+
 }
--- study/jpashop/src/main/java/jpagroup/jpashop/domain/Order.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/domain/Order.java
@@ -23,10 +23,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,6 +34,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    // 연관관계 메서드
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 
 }
