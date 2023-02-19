84d162e98197ebe979b6cc99f120359c8e7bca75
jay <dj.min43@gmail.com>
2023-02-19 16:42:44 +0900

    readonly

---
+++ study/jpashop/src/main/java/jpagroup/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpagroup.jpashop.service;
+
+import jpagroup.jpashop.domain.Member;
+import jpagroup.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+       memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
