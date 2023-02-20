acd3fe117bed0efc4d4a25aa64f0e3e431adf513
jay <dj.min43@gmail.com>
2023-02-20 12:46:37 +0900

    order service wip

--- study/jpashop/src/main/java/jpagroup/jpashop/domain/Order.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/domain/Order.java
@@ -51,5 +51,18 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //주문 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
 
 }
