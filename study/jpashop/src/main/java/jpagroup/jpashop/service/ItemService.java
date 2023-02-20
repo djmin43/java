322be895e96bf02d3a40b5afcb43408ddf81058c
jay <dj.min43@gmail.com>
2023-02-20 10:57:54 +0900

    create item service

---
+++ study/jpashop/src/main/java/jpagroup/jpashop/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpagroup.jpashop.service;
+
+import jpagroup.jpashop.domain.item.Item;
+import jpagroup.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+}
