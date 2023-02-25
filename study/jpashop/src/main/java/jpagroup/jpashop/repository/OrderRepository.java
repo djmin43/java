7917495f0c69cdb23540afb5e27b5c96295e4419
jay <dj.min43@gmail.com>
2023-02-25 17:28:53 +0900

    add order

--- study/jpashop/src/main/java/jpagroup/jpashop/repository/OrderRepository.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/repository/OrderRepository.java
@@ -1,2 +1,24 @@
-package jpagroup.jpashop.repository;public class OrderRepository {
+package jpagroup.jpashop.repository;
+
+import jpagroup.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
 }
--- study/jpashop/src/main/java/jpagroup/jpashop/service/OrderService.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/service/OrderService.java
@@ -1,2 +1,52 @@
-package jpagroup.jpashop.service;public class OrderService {
+package jpagroup.jpashop.service;
+
+import jpagroup.jpashop.domain.Delivery;
+import jpagroup.jpashop.domain.Member;
+import jpagroup.jpashop.domain.Order;
+import jpagroup.jpashop.domain.OrderItem;
+import jpagroup.jpashop.domain.item.Item;
+import jpagroup.jpashop.repository.ItemRepository;
+import jpagroup.jpashop.repository.MemberRepository;
+import jpagroup.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+      */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 조회
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+
+    // 취소
+
+    // 검색
 }
