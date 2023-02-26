1e2c2adf8f7363a061d6b398eb6904954f8536e8
jay <dj.min43@gmail.com>
2023-02-26 19:28:29 +0900

    test

--- study/jpashop/src/main/java/jpagroup/jpashop/service/OrderService.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/service/OrderService.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -45,7 +47,19 @@ public class OrderService {
         return order.getId();
     }
 
-    // 취소
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+
+    }
 
     // 검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
 }
---
+++ study/jpashop/src/test/java/jpagroup/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,84 @@
+package jpagroup.jpashop.service;
+
+import jpagroup.jpashop.domain.Address;
+import jpagroup.jpashop.domain.Member;
+import jpagroup.jpashop.domain.Order;
+import jpagroup.jpashop.domain.OrderStatus;
+import jpagroup.jpashop.domain.item.Book;
+import jpagroup.jpashop.domain.item.Item;
+import jpagroup.jpashop.repository.OrderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+
+        Item book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+
+        int orderCount = 2;
+
+        //when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다", 1, getOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다", 8, book.getStockQuantity());
+
+
+    }
+    @Test
+    public void 상품취소() throws Exception {
+        //given
+
+
+        //when
+
+
+        //then
+    }
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+
+
+        //when
+
+
+        //then
+    }
+
+}
