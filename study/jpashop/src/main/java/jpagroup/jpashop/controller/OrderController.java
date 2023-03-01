078875530a0ba87b8aad8336abd4a7bf40662b2b
jay <dj.min43@gmail.com>
2023-03-01 21:12:46 +0900

    order

---
+++ study/jpashop/src/main/java/jpagroup/jpashop/controller/OrderController.java
@@ -0,0 +1,56 @@
+package jpagroup.jpashop.controller;
+
+import jpagroup.jpashop.domain.Member;
+import jpagroup.jpashop.domain.Order;
+import jpagroup.jpashop.domain.item.Item;
+import jpagroup.jpashop.repository.MemberRepository;
+import jpagroup.jpashop.repository.OrderSearch;
+import jpagroup.jpashop.service.ItemService;
+import jpagroup.jpashop.service.MemberService;
+import jpagroup.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch,
+                            Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+}
--- study/jpashop/src/main/java/jpagroup/jpashop/service/OrderService.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jpagroup.jpashop.domain.item.Item;
 import jpagroup.jpashop.repository.ItemRepository;
 import jpagroup.jpashop.repository.MemberRepository;
 import jpagroup.jpashop.repository.OrderRepository;
+import jpagroup.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -59,7 +60,7 @@ public class OrderService {
     }
 
     // 검색
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+    }
 }
