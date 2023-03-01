87f38dab05267b21bca33cc8cd25efa5ca120f41
jay <dj.min43@gmail.com>
2023-03-01 21:15:12 +0900

    cancel order

--- study/jpashop/src/main/java/jpagroup/jpashop/controller/OrderController.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/controller/OrderController.java
@@ -11,10 +11,7 @@ import jpagroup.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -52,5 +49,11 @@ public class OrderController {
         List<Order> orders = orderService.findOrders(orderSearch);
         model.addAttribute("orders", orders);
         return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:orders";
     }
 }
