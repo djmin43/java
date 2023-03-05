80bf26fe04d0b8d8cf0cf54f54d7ae3e2f1b48da
jay <dj.min43@gmail.com>
2023-03-05 14:25:54 +0900

    do not expose entity to web

--- study/jpashop/src/main/java/jpagroup/jpashop/api/MemberApiController.java
+++ study/jpashop/src/main/java/jpagroup/jpashop/api/MemberApiController.java
@@ -2,9 +2,11 @@ package jpagroup.jpashop.api;
 
 import jpagroup.jpashop.domain.Member;
 import jpagroup.jpashop.service.MemberService;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,18 @@ import javax.validation.Valid;
 public class MemberApiController {
 
     private final MemberService memberService;
-    public CreateMemberReponse saveMemberV1(@RequestBody @Valid Member member) {
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
 
+    @Data
+    static class CreateMemberResponse {
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+
+        private Long id;
     }
 }
