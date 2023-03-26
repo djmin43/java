457ceef966ab97a51a483a0274c04d76a23b2438
jay <dj.min43@gmail.com>
2023-03-26 21:14:28 +0900

    create bag, ticekt, invitation

---
+++ object-book/src/main/java/org/example/Invitation.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ object-book/src/main/java/org/example/Ticket.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
