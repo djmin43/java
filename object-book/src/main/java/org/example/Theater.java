c7424485cd0fddd93569da1382ba650204f86c62
jay <dj.min43@gmail.com>
2023-03-28 07:39:08 +0900

    create a simple theater logic

---
+++ object-book/src/main/java/org/example/Theater.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        }
+        if (!audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+
+    }
+}
