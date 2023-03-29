a64a2445e25328c304b1dd7a37a5e580de348443
jay <dj.min43@gmail.com>
2023-03-29 11:49:15 +0900

    buy

--- object-book/src/main/java/org/example/Audience.java
+++ object-book/src/main/java/org/example/Audience.java
@@ -8,7 +8,14 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        }
+        bag.setTicket(ticket);
+        bag.minusAmount(ticket.getFee());
+        return ticket.getFee();
     }
 }
