f5be9bf49d94f67d80a73eb089bc767f33d4d07a
jay <dj.min43@gmail.com>
2023-03-29 11:45:29 +0900

    add note

--- object-book/src/main/java/org/example/Theater.java
+++ object-book/src/main/java/org/example/Theater.java
@@ -8,7 +8,7 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-
+        ticketSeller.sellTo(audience);
     }
 }
 
@@ -16,3 +16,7 @@ public class Theater {
 // 1. should function
 // 2. should be easy to change
 // 3. should be easy to comprehend
+
+// NOTE: 객체를 인터페이스와 구현으로 나누고,
+// 인터페이스만을 공개하는 것은 객체 사이의 결합도를 낮추고
+// 변경하기 쉬운 코드를 작성하기 위해 따라야 하는 가장 기본적인 설계 원칙이다. (pg. 21)
