3c6542b0f43bbee08673caaa3eca5cb699dd5487
jay <dj.min43@gmail.com>
2023-03-29 16:46:21 +0900

    more refactoring

--- object-book/src/main/java/org/example/Bag.java
+++ object-book/src/main/java/org/example/Bag.java
@@ -6,32 +6,26 @@ public class Bag {
     private Invitation invitation;
     private Ticket ticket;
 
-    public Bag(long amount) {
-        this(null, amount);
+
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        }
+        setTicket(ticket);
+        minusAmount(ticket.getFee());
+        return ticket.getFee();
     }
 
-    public Bag(Invitation invitation, long amount) {
-        this.invitation = invitation;
-        this.amount = amount;
-    }
-
-    public boolean hasInvitation() {
-        return invitation != null;
-    }
-
-    public boolean hasTicket() {
-        return ticket != null;
-    }
-
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
-        this.amount -= amount;
+    private boolean hasInvitation() {
+        return invitation != null;
     }
 
-    public void plusAmount(Long amount) {
-        this.amount += amount;
+    private void minusAmount(Long amount) {
+        this.amount -= amount;
     }
 }
--- object-book/src/main/java/org/example/TicketOffice.java
+++ object-book/src/main/java/org/example/TicketOffice.java
@@ -8,20 +8,17 @@ public class TicketOffice {
     private Long amount;
     private List<Ticket> tickets = new ArrayList<>();
 
-    public TicketOffice(Long amount, Ticket... tickets) {
-        this.amount = amount;
-        this.tickets.addAll(Arrays.asList(tickets));
+
+    public void sellTicketTo(Audience audience) {
+        plusAmount(audience.buy(getTicket()));
     }
 
-    public Ticket getTicket() {
+    private Ticket getTicket() {
         return tickets.remove(0);
     }
 
-    public void minusAmount(Long amount) {
-        this.amount -= amount;
-    }
-
-    public void plusAmount(Long amount) {
+    private void plusAmount(Long amount) {
         this.amount += amount;
     }
+
 }
