0f7ebbd386cfe13340b69cb1c50826e4040de658
Jay Min <dj.min43@gmail.com>
2023-04-18 07:12:53 +0900

    test works

--- jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -2,7 +2,7 @@ package hello.jdbc.connection;
 
 public abstract class ConnectionConst {
 
-    public static final String URL = "jdbc:h2:tcp://localhost/~/jdbc-test";
+    public static final String URL = "jdbc:h2:tcp://localhost/~/jdbc-basic";
     public static final String USERNAME = "sa";
     public static final String PASSWORD = "";
 }
