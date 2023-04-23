c09e74b80f26ab493a9e4a223c5da7a8b03d06fb
jay <dj.min43@gmail.com>
2023-04-23 15:02:41 +0900

    add assert code

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,10 +2,12 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -16,11 +18,12 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         // save
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
         
         // findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
 }
