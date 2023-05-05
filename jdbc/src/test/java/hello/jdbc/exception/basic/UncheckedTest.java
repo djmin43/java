f468cc86623bbb8b69e830798899adbce5dc1a87
jay <dj.min43@gmail.com>
2023-05-05 10:51:09 +0900

    call catch

--- jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -1,5 +1,8 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class UncheckedTest {
 
     /**
@@ -10,6 +13,27 @@ public class UncheckedTest {
             super(message);
         }
     }
+
+    /**
+     * Unchecked 예외는 예뢰를 잡거나, 던지지 않아도 된다.
+     * 예외를 잡지 않으면 자동으로 밖으로 던진다.
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        /**
+         * 필요한 경우 예외를 잡아서 처리하면 된다.
+         */
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyUncheckedException e) {
+                //예외 처리 로직
+                log.info("예외 처리, Message={}", e.getMessage(), e);
+            }
+        }
+    }
+
 
     static class Repository {
         public void call() {
