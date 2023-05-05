ee1d1af8ead12adda84d806c9a43339af742e417
jay <dj.min43@gmail.com>
2023-05-05 10:54:21 +0900

    throws

--- jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -1,9 +1,24 @@
 package hello.jdbc.exception.basic;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class UncheckedTest {
+
+    @Test
+    void unchecked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(() -> service.callThrow())
+                .isInstanceOf(RuntimeException.class);
+    }
 
     /**
      * RuntimeException을 상속받은 예외는 언체크 예외가 된다.
@@ -31,6 +46,14 @@ public class UncheckedTest {
                 //예외 처리 로직
                 log.info("예외 처리, Message={}", e.getMessage(), e);
             }
+        }
+
+        /**
+         * 예외를 잡지 않아도 된다. 자연스럽게 상위로 넘어간다.
+         * 체크 예외와 다르게 throws 예외 선언을 하지 않아도 된다.
+         */
+        public void callThrow() {
+            repository.call();
         }
     }
 
