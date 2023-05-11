76d74b60f1313823d1ee5345ef4f2cafbd00493f
jay <dj.min43@gmail.com>
2023-05-11 19:56:20 +0900

    remove interface

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -16,7 +16,7 @@ import java.util.NoSuchElementException;
  * DataSourceUtils.releaseConnection()
  */
 @Slf4j
-public class MemberRepositoryV3 implements MemberRepositoryEx {
+public class MemberRepositoryV3 {
 
     private final DataSource dataSource;
 
