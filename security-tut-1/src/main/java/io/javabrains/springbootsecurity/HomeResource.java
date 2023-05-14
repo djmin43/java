4ea1e85350f171d9a4cffd57f62c9aabff314e6d
jay <dj.min43@gmail.com>
2023-05-14 10:13:16 +0900

    clone tutorial 1

---
+++ security-tut-1/src/main/java/io/javabrains/springbootsecurity/HomeResource.java
@@ -0,0 +1,23 @@
+package io.javabrains.springbootsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h1>Welcome</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>");
+    }
+}
