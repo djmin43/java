e4409fa6b367b6a17975f81485ffad6163b56229
jay <dj.min43@gmail.com>
2023-05-14 10:15:30 +0900

    init

--- spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/HomeResource.java
+++ spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/HomeResource.java
@@ -11,13 +11,4 @@ public class HomeResource {
         return ("<h1>Welcome</h1>");
     }
 
-    @GetMapping("/user")
-    public String user() {
-        return ("<h1>Welcome User</h1>");
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return ("<h1>Welcome Admin</h1>");
-    }
 }
