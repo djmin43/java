74405b4ef74fade728ba651f33f7fd2d758bc626
jay <dj.min43@gmail.com>
2023-05-18 21:03:33 +0900

    get security config

--- spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
+++ spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
@@ -1,13 +1,27 @@
 package io.javabrains.springbootsecurity;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class SecurityConfiguration extends WebSecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        // set your configuration on the auth objects
-
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // set  your configuration on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER");
     }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
