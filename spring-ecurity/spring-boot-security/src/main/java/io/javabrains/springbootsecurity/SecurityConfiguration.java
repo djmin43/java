e27201dd707894aa05d109005c223535e437d22d
jay <dj.min43@gmail.com>
2023-05-20 12:13:55 +0900

    configure auth

--- spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
+++ spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/").permitAll()
                 .and().formLogin();
     }
