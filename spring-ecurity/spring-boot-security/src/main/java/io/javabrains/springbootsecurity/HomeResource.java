32bfa10afdf416e8fcf4ef7a622383e103803456
jay <dj.min43@gmail.com>
2023-05-20 10:19:51 +0900

    configure http

--- spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/HomeResource.java
+++ spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/HomeResource.java
@@ -10,5 +10,13 @@ public class HomeResource {
     public String home() {
         return ("<h1>Welcome</h1>");
     }
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>");
+    }
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User</h1>");
+    }
 
 }
--- spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
+++ spring-ecurity/spring-boot-security/src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package io.javabrains.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -21,6 +22,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("foo")
                 .password("foo")
                 .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").hasRole("ADMIN")
+                .and().formLogin();
     }
 
     @Bean
