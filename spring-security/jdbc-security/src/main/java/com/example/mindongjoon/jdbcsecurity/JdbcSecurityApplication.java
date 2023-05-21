a77f43c2114865fc996ff5c02ad05d7344b3574f
jay <dj.min43@gmail.com>
2023-05-21 17:16:13 +0900

    name fix

---
+++ spring-security/jdbc-security/src/main/java/com/example/mindongjoon/jdbcsecurity/JdbcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindongjoon.jdbcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcSecurityApplication.class, args);
+	}
+
+}
---
+++ spring-security/jdbc-security/src/test/java/com/example/mindongjoon/jdbcsecurity/JdbcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindongjoon.jdbcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
