cc81d8b8c112d22fe7eb1f858414fb5290c96246
jay <dj.min43@gmail.com>
2023-05-21 17:53:09 +0900

    jpa security init

---
+++ spring-security/JPA-security/src/test/java/com/example/mindongjoon/JPA/security/JpaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindongjoon.JPA.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
