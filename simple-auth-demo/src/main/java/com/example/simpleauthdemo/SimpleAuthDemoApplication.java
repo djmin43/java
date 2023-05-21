0b2127726ee7a6dc3e188e4024408d139fef4a9f
Jay Min <dj.min43@gmail.com>
2023-05-22 06:47:52 +0900

    auth init

---
+++ simple-auth-demo/src/main/java/com/example/simpleauthdemo/SimpleAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuthDemoApplication.class, args);
+	}
+
+}
---
+++ simple-auth-demo/src/test/java/com/example/simpleauthdemo/SimpleAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
