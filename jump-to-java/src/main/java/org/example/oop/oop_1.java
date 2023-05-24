4bbf9e1cf886af357034c7937cde0b6d417807f4
Jay Min <dj.min43@gmail.com>
2023-05-25 07:37:00 +0900

    oop 1

---
+++ jump-to-java/src/main/java/org/example/oop/oop_1.java
@@ -0,0 +1,19 @@
+package org.example.oop;
+
+public class oop_1 {
+
+    class Calculator {
+        static int result = 0;
+
+        static int add(int num) {
+            result += num;
+            return result;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(3));
+        System.out.println(Calculator.add(4));
+    }
+}
