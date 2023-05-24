d8e4e1a30d2ffb8a13fd9292cf7574c7e437e1f3
Jay Min <dj.min43@gmail.com>
2023-05-25 07:25:59 +0900

    control done

--- jump-to-java/src/main/java/org/example/controls/Quiz.java
+++ jump-to-java/src/main/java/org/example/controls/Quiz.java
@@ -4,7 +4,60 @@ public class Quiz {
 
     public static void main(String[] args) {
         System.out.println("helo");
+        two();
+        three();
+        four();
+        five();
     }
 
-    static one
+    static void one() {
+        // answer should be everywhere because it has no everywhere
+        String a = "write once, run anywhere";
+        if (a.contains("wife")) {
+            System.out.println("wife");
+        } else if (a.contains("once") && !a.contains("run")) {
+            System.out.println("once");
+        } else if (!a.contains("everywhere")) {
+            System.out.println("everywhere");
+        } else if (a.contains("anywhere")) {
+            System.out.println("anywhere");
+        } else {
+            System.out.println("none");
+        }
+    }
+
+    static void two() {
+        int sum = 0;
+        int start = 1;
+        while (start <= 1000) {
+            if (start % 3 == 0) {
+                sum = sum + start;
+            }
+            start++;
+        }
+        System.out.println("sum = " + sum);
+        // answer = 166833
+    }
+
+    static void three() {
+        for (int i = 0; i < 7; i++) {
+            String stars = "*".repeat(i);
+            System.out.println(stars);
+        }
+    }
+
+    static void four() {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+    static void five() {
+        int[] marks = {70, 60, 55, 75, 95, 90, 80, 80, 85, 100};
+        int total = 0;
+        for (int mark : marks) {
+            total = total + mark;
+        }
+        System.out.println("average = " + (total / marks.length));
+    }
 }
