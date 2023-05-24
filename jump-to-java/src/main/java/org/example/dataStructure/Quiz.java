853b55b314cf1e8a642b425ab5fdc1ce776f26a8
jay <dj.min43@gmail.com>
2023-05-24 20:59:16 +0900

    data structure done

--- jump-to-java/src/main/java/org/example/dataStructure/Quiz.java
+++ jump-to-java/src/main/java/org/example/dataStructure/Quiz.java
@@ -1,14 +1,11 @@
 package org.example.dataStructure;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class Quiz {
 
     public static void main(String[] args) {
-        Six();
+        Last();
 
     }
 
@@ -49,7 +46,48 @@ public class Quiz {
         System.out.println("myList = " + myList);
     }
 
+    static void Seven() {
+        ArrayList<String> myList = new ArrayList<>(Arrays.asList("Life", "is", "too", "short"));
+        String join = String.join(" ", myList);
+        System.out.println("join = " + join);
+    }
 
+    static void Eight() {
+        HashMap<String, Integer> grade = new HashMap<>();
+        grade.put("A", 90);
+        grade.put("B", 80);
+        grade.put("C", 70);
+
+        grade.remove("B");
+        System.out.println("grade = " + grade);
+    }
+
+    static void Nine() {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5));
+        HashSet newNumbers = new HashSet(Arrays.asList(numbers.toArray()));
+        System.out.println("newNumbers = " + newNumbers);
+
+    }
+
+    enum CoffeeEnum {
+        AMERICANO,
+        ICED_AMERICANO,
+        CAFE_LATTE
+
+    }
+
+    static void printCoffeePrice(CoffeeEnum type) {
+        HashMap<CoffeeEnum, Integer> priceMap = new HashMap<>();
+        priceMap.put(CoffeeEnum.AMERICANO, 3000);  // 1: 아메리카노
+        priceMap.put(CoffeeEnum.ICED_AMERICANO, 4000);  // 2: 아이스 아메리카노
+        priceMap.put(CoffeeEnum.CAFE_LATTE, 5000);  // 3: 카페라떼
+        int price = priceMap.get(type);
+        System.out.println(String.format("가격은 %d원 입니다.", price));
+    }
+
+    static void Last() {
+        printCoffeePrice(CoffeeEnum.AMERICANO);
+    }
 
 
 
