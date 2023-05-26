73961ef32fa12e4f0aa1018ac1f1b9f501e547a3
jay <dj.min43@gmail.com>
2023-05-26 15:24:58 +0900

    interface!

--- jump-to-java/src/main/java/org/example/oop/Sample.java
+++ jump-to-java/src/main/java/org/example/oop/Sample.java
@@ -8,54 +8,22 @@ class Animal {
     }
 }
 
-class Dog extends Animal {  // Animal 클래스를 상속한다.
-
-
-    void sleep() {
-        System.out.println(this.name + " zzz");
-    }
+class Tiger extends Animal {
 }
 
-enum DogType {
-    DOBERMAN,
-    POODLE,
-
+class Lion extends Animal {
 }
 
-class HouseDog extends Dog {
-
-    HouseDog(String name) {
-        this.name = name;
+class ZooKeeper {
+    void feed(Tiger tiger) {  // 호랑이가 오면 사과를 던져 준다.
+        System.out.println("feed apple");
     }
 
-    HouseDog(DogType type) {
-        if (type == DogType.POODLE) {
-            this.name = "poodle";
-        } else if (type == DogType.DOBERMAN) {
-            this.name = "doberman";
-        }
-    }
-
-    void sleep() {
-        System.out.println(this.name + " zzz in house");
-    }
-
-    void sleep(int hour) {
-        System.out.println(this.name+" zzz in house for " + hour + " hours");
+    void feed(Lion lion) {  // 사자가 오면 바나나를 던져준다.
+        System.out.println("feed banana");
     }
 }
-
-
-
 public class Sample {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.setName("poppy");
-        dog.sleep();
-        HouseDog houseDog = new HouseDog("happy");
-        houseDog.sleep();
-        houseDog.sleep(5);
-        HouseDog doberman = new HouseDog(DogType.DOBERMAN);
-        doberman.sleep();
     }
 }
