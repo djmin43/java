dc78d9dc8e3c964c13489b8710c27ad02ea9e0b6
jay <dj.min43@gmail.com>
2023-05-26 16:09:35 +0900

    cal

--- jump-to-java/src/main/java/org/example/oop/Quiz.java
+++ jump-to-java/src/main/java/org/example/oop/Quiz.java
@@ -14,6 +14,10 @@ class Calculator {
     int getValue() {
         return this.value;
     }
+
+    boolean isOdd(int val) {
+        return val % 2 != 0;
+    }
 }
 
 class UpgradeCalculator extends Calculator {
@@ -22,12 +26,20 @@ class UpgradeCalculator extends Calculator {
     }
 }
 
+class MaxLimitCalculator extends Calculator {
+    void add(int val) {
+        this.value += val;
+        if (this.value > 100) {
+            this.value = 100;
+        }
+    }
+}
+
 public class Quiz {
 
     public static void main(String[] args) {
-        UpgradeCalculator cal = new UpgradeCalculator();
-        cal.add(10);
-        cal.minus(3);
-        System.out.println(cal.getValue());
+        Calculator cal = new Calculator();
+        System.out.println(cal.isOdd(3));  // 3은 홀수이므로 true 출력
+        System.out.println(cal.isOdd(4));  // 4는 짝수이므로 false 출력
     }
 }
