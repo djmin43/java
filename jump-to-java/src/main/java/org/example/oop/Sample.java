3abcb83150d09b59bd645f4ff6260123103904b2
jay <dj.min43@gmail.com>
2023-05-26 15:02:30 +0900

    constructor override

--- jump-to-java/src/main/java/org/example/oop/Sample.java
+++ jump-to-java/src/main/java/org/example/oop/Sample.java
@@ -9,15 +9,31 @@ class Animal {
 }
 
 class Dog extends Animal {  // Animal 클래스를 상속한다.
+
+
     void sleep() {
         System.out.println(this.name + " zzz");
     }
+}
+
+enum DogType {
+    DOBERMAN,
+    POODLE,
+
 }
 
 class HouseDog extends Dog {
 
     HouseDog(String name) {
         this.name = name;
+    }
+
+    HouseDog(DogType type) {
+        if (type == DogType.POODLE) {
+            this.name = "poodle";
+        } else if (type == DogType.DOBERMAN) {
+            this.name = "doberman";
+        }
     }
 
     void sleep() {
@@ -39,5 +55,7 @@ public class Sample {
         HouseDog houseDog = new HouseDog("happy");
         houseDog.sleep();
         houseDog.sleep(5);
+        HouseDog doberman = new HouseDog(DogType.DOBERMAN);
+        doberman.sleep();
     }
 }
