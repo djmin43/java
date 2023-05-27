d4fda80834252cb5e7dc1692a9605332b1f11035
jay <dj.min43@gmail.com>
2023-05-27 14:12:52 +0900

    thread

--- jump-to-java/src/main/java/org/example/more/quiz/Sample.java
+++ jump-to-java/src/main/java/org/example/more/quiz/Sample.java
@@ -1,10 +1,17 @@
 package org.example.more.quiz;
 
-class HeavyWork {
+import java.util.ArrayList;
+
+class HeavyWork implements Runnable {
     String name;
 
     HeavyWork(String name) {
         this.name = name;
+    }
+
+    @Override
+    public void run() {
+        work();
     }
 
     public void work() {
@@ -19,11 +26,16 @@ class HeavyWork {
 }
 
 public class Sample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         long start = System.currentTimeMillis();
+        ArrayList<Thread> threads = new ArrayList<>();
         for (int i = 1; i < 5; i++) {
-            HeavyWork w = new HeavyWork("w" + i);
-            w.work();
+            Thread thread = new Thread(new HeavyWork("w" + i));
+            threads.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
         }
         long end = System.currentTimeMillis();
         System.out.printf("elapsed time:%s ms\n", end - start);
