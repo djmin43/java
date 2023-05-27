5a1c977d7732dbaeea25ec82d0504b60d5ced28e
jay <dj.min43@gmail.com>
2023-05-27 15:29:56 +0900

    sbb init

---
+++ sbb/src/test/java/com/example/mindongjoon/sbb/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindongjoon.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
