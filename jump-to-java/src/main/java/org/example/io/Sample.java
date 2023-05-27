b672e0897a8c0a4939bf1166b2277a56a933afc1
jay <dj.min43@gmail.com>
2023-05-27 11:12:37 +0900

    abc

--- jump-to-java/src/main/java/org/example/io/Sample.java
+++ jump-to-java/src/main/java/org/example/io/Sample.java
@@ -9,8 +9,15 @@ public class Sample {
         InputStream in = System.in;
 
         int a;
+        int b;
+        int c;
         a = in.read();
+        b = in.read();
+        c = in.read();
+
 
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
