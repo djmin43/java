0384d8d4bc7650c357e8531b77aad0b0dda96a52
jay <dj.min43@gmail.com>
2023-05-27 11:39:56 +0900

    io done

--- jump-to-java/src/main/java/org/example/io/Sample.java
+++ jump-to-java/src/main/java/org/example/io/Sample.java
@@ -1,15 +1,10 @@
 package org.example.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Sample {
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.next());
     }
 }
