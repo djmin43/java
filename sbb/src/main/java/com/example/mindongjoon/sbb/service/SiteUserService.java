30e6e0f198f59ecdf77f5d0b501e454015a3eb7b
jay <dj.min43@gmail.com>
2023-05-27 19:33:52 +0900

    site user

--- sbb/src/main/java/com/example/mindongjoon/sbb/service/SiteUserService.java
+++ sbb/src/main/java/com/example/mindongjoon/sbb/service/SiteUserService.java
@@ -4,6 +4,7 @@ import com.example.mindongjoon.sbb.domain.SiteUser;
 import com.example.mindongjoon.sbb.repository.SiteUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,12 +12,12 @@ import org.springframework.stereotype.Service;
 public class SiteUserService {
 
     private final SiteUserRepository siteUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.siteUserRepository.save(user);
         return user;
