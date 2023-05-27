047099589dcc75c4cec7474e43bf6aeffbd4d3ba
jay <dj.min43@gmail.com>
2023-05-27 13:58:18 +0900

    bi function

--- jump-to-java/src/main/java/org/example/more/functional/Sample.java
+++ jump-to-java/src/main/java/org/example/more/functional/Sample.java
@@ -1,23 +1,22 @@
 package org.example.more.functional;
 
-@FunctionalInterface
-interface Calculator {
-    int sum(int a, int b);
-}
+//@FunctionalInterface
+//interface Calculator {
+//    int sum(int a, int b);
+//}
 
-class MyCalculator implements Calculator {
-    public int sum(int a, int b) {
-        return a+b;
-    }
-}
+//class MyCalculator implements Calculator {
+//    public int sum(int a, int b) {
+//        return a+b;
+//    }
+//}
+
+import java.util.function.BiFunction;
 
 public class Sample {
     public static void main(String[] args) {
-        MyCalculator mc = new MyCalculator();
-        Calculator nc = (int a, int b) -> a + b;
-        int sum = nc.sum(3, 5);
-        System.out.println("sum = " + sum);
-        int result = mc.sum(3, 4);
+        BiFunction<Integer, Integer, Integer> mc = (a, b) -> a + b;
+        int result = mc.apply(3, 4);
         System.out.println("result = " + result);
     }
 }
