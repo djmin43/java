bd521975cf3f83451d694b37918f4fa14de327cc
jay <dj.min43@gmail.com>
2023-05-27 15:55:29 +0900

    sbb init

---
+++ sbb/src/main/java/com/example/mindongjoon/sbb/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.mindongjoon.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello Worldx1";
+    }
+}
