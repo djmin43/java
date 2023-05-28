fac71e8976ace7c0901a7d44841ee024a731996f
jay <dj.min43@gmail.com>
2023-05-28 16:44:48 +0900

    oauth init

---
+++ jwt-oauth/src/main/java/com/example/jwtoauth/JwtOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtOauthApplication.class, args);
+	}
+
+}
---
+++ jwt-oauth/src/test/java/com/example/jwtoauth/JwtOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
