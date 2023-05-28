f32d755e007297ee13405f1287cdc674ff790de8
jay <dj.min43@gmail.com>
2023-05-28 15:05:26 +0900

    .

--- security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtAuthenticationFilter.java
+++ security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NotNull HttpServletRequest request,
@@ -32,6 +36,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // position 7 comes from Bearer with space
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadByUsername(userEmail);
+
+        }
 
     }
 }
--- security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
+++ security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
@@ -45,6 +45,19 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parserBuilder()
