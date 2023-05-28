0f29d4a3452ec4db97636df810c335a781376a50
jay <dj.min43@gmail.com>
2023-05-28 11:11:03 +0900

    entity

---
+++ security-jwt/src/main/java/com/mindongjoon/securityjwt/user/User.java
@@ -0,0 +1,24 @@
+package com.mindongjoon.securityjwt.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
