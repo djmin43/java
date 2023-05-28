94f9b116c0a1108fde77cd4b81cf85dd5fb7d72d
jay <dj.min43@gmail.com>
2023-05-28 11:54:35 +0900

    filter

---
+++ security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.mindongjoon.securityjwt.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
