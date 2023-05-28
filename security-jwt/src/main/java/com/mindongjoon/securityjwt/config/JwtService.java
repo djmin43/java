9d26803caffac8bf72404ca30a82034161a8be2f
jay <dj.min43@gmail.com>
2023-05-28 13:36:22 +0900

    generate token

--- security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
+++ security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
@@ -2,11 +2,16 @@ package com.mindongjoon.securityjwt.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -17,9 +22,27 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .compact();
     }
 
     private Claims extractAllClaims(String token) {
