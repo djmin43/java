f6e2a37b2bfba0ea96a0dbcd28fb9608ceebcd5c
jay <dj.min43@gmail.com>
2023-05-28 13:27:38 +0900

    jwt keys

--- security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
+++ security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
@@ -1,10 +1,33 @@
 package com.mindongjoon.securityjwt.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "76397924423F4528482B4D6251655468576D5A7134743777217A25432A46294A";
     public String extractUsername(String token) {
         return null;
     }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
