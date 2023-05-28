e3951ae5271ec9052dcf23469507c8b4b1c724ff
jay <dj.min43@gmail.com>
2023-05-28 13:29:54 +0900

    jwt

--- security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
+++ security-jwt/src/main/java/com/mindongjoon/securityjwt/config/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,6 +15,11 @@ public class JwtService {
     private static final String SECRET_KEY = "76397924423F4528482B4D6251655468576D5A7134743777217A25432A46294A";
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token) {
