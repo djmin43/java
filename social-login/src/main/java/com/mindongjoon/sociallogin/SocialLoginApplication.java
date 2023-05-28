6eab3de769979235ecaefad03273762885286aa5
jay <dj.min43@gmail.com>
2023-05-28 15:07:56 +0900

    social login init

---
+++ social-login/src/main/java/com/mindongjoon/sociallogin/SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.mindongjoon.sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginApplication.class, args);
+	}
+
+}
