a1f7fe656e4fed564c759bc0efbc61cef7543b3a
jay <dj.min43@gmail.com>
2023-05-29 20:41:07 +0900

    init

---
+++ security-endgame/src/test/java/com/mindongjoon/securityendgame/SecurityEndgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindongjoon.securityendgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityEndgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
