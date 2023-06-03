cb0034351e66c71d63beaa6e3d46d89b907e8ff3
jay <dj.min43@gmail.com>
2023-06-03 21:48:57 +0900

    request matchers

--- security-endgame/src/main/java/com/mindongjoon/securityendgame/config/SecurityConfig.java
+++ security-endgame/src/main/java/com/mindongjoon/securityendgame/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mindongjoon.securityendgame.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,11 +27,11 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults())
                 .build();
     }
-
 
 }
