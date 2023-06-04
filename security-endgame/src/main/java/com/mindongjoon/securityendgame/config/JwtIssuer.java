ef71d03ed4380db23ea4cea13c24d960e693482b
jay <dj.min43@gmail.com>
2023-06-04 11:35:16 +0900

    create a issue token

---
+++ security-endgame/src/main/java/com/mindongjoon/securityendgame/config/JwtIssuer.java
@@ -0,0 +1,23 @@
+package com.mindongjoon.securityendgame.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Component
+public class JwtIssuer {
+
+    public String issue(Long userId, String email, List<String> roles) {
+        return JWT.create()
+                .withSubject(String.valueOf(userId))
+                .withExpiresAt(Instant.now().plus(Duration.of(1, ChronoUnit.DAYS)))
+                .withClaim("e", email)
+                .withClaim("a", roles)
+                .sign(Algorithm.HMAC256("secret"));
+    }
+}
