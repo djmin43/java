43c3b4669882612919b19411f4acc3a6a3d73b8d
jay <dj.min43@gmail.com>
2023-06-04 11:42:40 +0900

    implement jwt token

--- security-endgame/src/main/java/com/mindongjoon/securityendgame/controller/AuthController.java
+++ security-endgame/src/main/java/com/mindongjoon/securityendgame/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.mindongjoon.securityendgame.controller;
 
+import com.mindongjoon.securityendgame.config.JwtIssuer;
 import com.mindongjoon.securityendgame.model.LoginRequest;
 import com.mindongjoon.securityendgame.model.LoginResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final JwtIssuer jwtIssuer;
+
     @PostMapping("/auth/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request) {
+        var token = jwtIssuer.issue(1L, request.getEmail(), List.of("USER"));
         return LoginResponse.builder()
                 .accessToken("blah")
                 .build();
