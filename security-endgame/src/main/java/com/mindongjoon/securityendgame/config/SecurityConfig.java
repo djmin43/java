32ffcc413a78ce604b630381bd6be19aa1572dcf
jay <dj.min43@gmail.com>
2023-06-04 11:13:55 +0900

    fix security

---
+++ security-endgame/src/main/java/com/mindongjoon/securityendgame/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.mindongjoon.securityendgame.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+        return http
+                .cors().disable()
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(login -> login.disable())
+                .securityMatcher("/**")
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                        )
+                .build();
+    }
+}
