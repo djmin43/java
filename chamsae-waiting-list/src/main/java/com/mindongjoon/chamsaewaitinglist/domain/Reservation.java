58302da9275d90ebacf816164a506fedeb4336a5
James Min <james.min@freedgrouptech.com>
2023-06-22 07:46:40 +0900

    create new reservation

---
+++ chamsae-waiting-list/src/main/java/com/mindongjoon/chamsaewaitinglist/domain/Reservation.java
@@ -0,0 +1,42 @@
+package com.mindongjoon.chamsaewaitinglist.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private Boolean isTermsAgreed;
+    private Boolean isResolved;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+    private Reservation(String name, String phoneNumber, Boolean isTermsAgreed, Boolean isResolved) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.isTermsAgreed = isTermsAgreed;
+        this.isResolved = isResolved;
+    }
+
+    public static Reservation createNewReservation(String name, String phoneNumber, Boolean isTermsAgreed, Boolean isResolved) {
+        return new Reservation(name, phoneNumber, isTermsAgreed, isResolved);
+    }
+}
