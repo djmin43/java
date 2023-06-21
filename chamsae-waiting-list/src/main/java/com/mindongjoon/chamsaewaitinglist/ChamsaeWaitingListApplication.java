099cc71e54e1cce6c45495f97788827a1ff037f4
James Min <james.min@freedgrouptech.com>
2023-06-22 07:00:02 +0900

    chamsae init

---
+++ chamsae-waiting-list/src/main/java/com/mindongjoon/chamsaewaitinglist/ChamsaeWaitingListApplication.java
@@ -0,0 +1,13 @@
+package com.mindongjoon.chamsaewaitinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamsaeWaitingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamsaeWaitingListApplication.class, args);
+	}
+
+}
