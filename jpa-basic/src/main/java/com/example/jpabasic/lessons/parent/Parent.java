2cf4025177fdfa5b661d1bc1dc0a845b0fb9afa3
jay <dj.min43@gmail.com>
2023-07-15 11:18:13 +0900

    examples

--- jpa-basic/src/main/java/com/example/jpabasic/lessons/parent/Parent.java
+++ jpa-basic/src/main/java/com/example/jpabasic/lessons/parent/Parent.java
@@ -3,10 +3,7 @@ package com.example.jpabasic.lessons.parent;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +17,7 @@ public class Parent {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "parent")
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
     private List<Child> childList = new ArrayList<>();
 
     public void addChild(Child child) {
