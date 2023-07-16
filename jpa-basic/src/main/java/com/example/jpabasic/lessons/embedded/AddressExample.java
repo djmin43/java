2b82775e27d3659486e2ac74dac43c06f15efaa5
jay <dj.min43@gmail.com>
2023-07-16 09:33:02 +0900

    equals

--- jpa-basic/src/main/java/com/example/jpabasic/lessons/embedded/AddressExample.java
+++ jpa-basic/src/main/java/com/example/jpabasic/lessons/embedded/AddressExample.java
@@ -3,6 +3,7 @@ package com.example.jpabasic.lessons.embedded;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 @Data
@@ -11,4 +12,17 @@ public class AddressExample {
     private String city;
     private String zipcode;
     private String street;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AddressExample that = (AddressExample) o;
+        return Objects.equals(city, that.city) && Objects.equals(zipcode, that.zipcode) && Objects.equals(street, that.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, zipcode, street);
+    }
 }
