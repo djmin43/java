b05f73c50cdea5c3a55b84a0750ee9a097404b1f
jay <dj.min43@gmail.com>
2023-07-29 12:26:48 +0900

    gauss sum

--- doit-algo/src/main/java/org/example/chapter1/SumPractice.java
+++ doit-algo/src/main/java/org/example/chapter1/SumPractice.java
@@ -8,30 +8,27 @@ public class SumPractice {
         Scanner stdIn = new Scanner(System.in);
 
         int input = stdIn.nextInt();
-
         sumAll(input);
+        gaussSum(input);
+    }
 
+    private static void gaussSum(int n) {
+        int sum = n * (n + 1) / 2;
+        System.out.println("gauss sum = " + sum);
     }
 
     private static void sumAll(int input) {
-
         int sum = 0;
-
         StringBuilder sb = new StringBuilder();
-
         for (int i = 1; i <= input; i++) {
             sum += i;
             sb.append(" ").append(i).append(" ");
-
             if (i < input) {
                 sb.append("+");
             }
         }
-
         sb.append("= ").append(sum);
-
         String finalString = sb.toString();
-
         System.out.println(finalString);
     }
 }
