4cb5b9e964afdd5fae4b4d9e449c34944651ca83
jay <dj.min43@gmail.com>
2023-07-30 21:04:41 +0900

    bubble sort

--- doit-algo/src/main/java/org/example/chapter6/BubbleSort.java
+++ doit-algo/src/main/java/org/example/chapter6/BubbleSort.java
@@ -15,10 +15,15 @@ public class BubbleSort {
 
     private static int[] bubbleSort(int[] a) {
         for (int i = 0; i < a.length - 1; i++) {
+            int exchange = 0;
             for (int j = 0; j < a.length - 1 - i; j++) {
                 if (a[j] > a[j + 1]) {
                     swap(a, j, j+1);
+                    exchange++;
                 }
+            }
+            if (exchange == 0) {
+                break;
             }
         }
 
