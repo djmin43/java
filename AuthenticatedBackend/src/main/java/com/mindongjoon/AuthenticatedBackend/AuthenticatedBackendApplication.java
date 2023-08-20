9a488d96c9544ad30352532a16567682affb0269
jay <dj.min43@gmail.com>
2023-08-20 20:54:02 +0900

    auth backend init

---
+++ AuthenticatedBackend/src/main/java/com/mindongjoon/AuthenticatedBackend/AuthenticatedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindongjoon.AuthenticatedBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatedBackendApplication.class, args);
+	}
+
+}
