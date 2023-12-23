bbe9366fecb1702ec41853c2746fd7b6fd469f39
dongjoon min <dj.min43@gmail.com>
2023-12-23 20:19:56 +0900

    deque until iterator

--- hello/Deque.java
+++ hello/Deque.java
@@ -7,66 +7,95 @@ import java.util.Iterator;
  **************************************************************************** */
 public class Deque<Item> implements Iterable<Item> {
 
+    public Node first, last;
+    public int count;
 
-    public Item[] deque;
+    private class Node {
+        Item item;
+        Node next;
+    }
+
 
     // construct an empty deque
     public Deque() {
-        this.deque = (Item[]) new Object[0];
+        this.first = null;
+        this.last = null;
+        this.count = 0;
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-        for (int i = 0; i < deque.length; i++) {
-            if (deque[i] != null) {
-                return false;
-            }
-        }
-        return true;
+        return this.first.item == null;
     }
 
     // return the number of items on the deque
     public int size() {
-        int dequeSize = 0;
-        for (int i = 0; i < deque.length; i++) {
-            if (deque[i] != null) {
-                dequeSize++;
-            }
-        }
-        return dequeSize;
+        return count;
     }
 
     // add the item to the front
     public void addFirst(Item item) {
-        if ()
+        Node oldFirst = this.first;
+        Node node = new Node();
+        node.item = item;
+        node.next = oldFirst;
+        this.first = node;
+        count++;
     }
 
     // add the item to the back
     public void addLast(Item item) {
+        Node oldLast = this.last;
+        Node node = new Node();
+        node.item = item;
+        node.next = null;
+        if (isEmpty()) {
+            this.first = node;
+        }
+        else {
+            oldLast.next = node;
+        }
+        count++;
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node oldFirst = this.first;
+        Node node = oldFirst.next;
+        this.first = node;
+        count--;
+        return this.first.item;
     }
 
     // remove and return the item from the back
     public Item removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node temp = this.first;
+        while (temp.next != null) {
+            System.out.println("temp.item = " + temp.item);
+            this.last = temp;
+            temp = temp.next;
+        }
+        this.last.next = null;
+        count--;
+        return this.last.item;
     }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
+        Iterator<Item> iterator = new Deque<Item>().iterator();
+        return iterator;
     }
 
     // unit testing (required)
     public static void main(String[] args) {
-
+        Deque<String> deque = new Deque<String>();
+        int size = deque.size();
     }
 
-    private void resize(int capacity) {
-        Item[] copy = (Item[]) new Object[capacity];
-        for (int i = 0; i < deque.length; i++) {
-            copy[i] = deque[i];
-        }
-        deque = copy;
-    }
 }
--- hello/Percolation.java
+++ hello/Percolation.java
@@ -1,39 +1,39 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
-
-public class Percolation {
-
-
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
-
-    }
-
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
-    }
-
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-    }
-
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-
-    }
-
-    // returns the number of open sites
-    public int numberOfOpenSites() {
-
-    }
-
-
-    // does the system percolate?
-    public boolean percolates() {
-
-    }
-
-}
+// /* *****************************************************************************
+//  *  Name:
+//  *  Date:
+//  *  Description:
+//  **************************************************************************** */
+//
+// public class Percolation {
+//
+//
+//     // creates n-by-n grid, with all sites initially blocked
+//     public Percolation(int n) {
+//
+//     }
+//
+//     // opens the site (row, col) if it is not open already
+//     public void open(int row, int col) {
+//     }
+//
+//     // is the site (row, col) open?
+//     public boolean isOpen(int row, int col) {
+//     }
+//
+//     // is the site (row, col) full?
+//     public boolean isFull(int row, int col) {
+//
+//     }
+//
+//     // returns the number of open sites
+//     public int numberOfOpenSites() {
+//
+//     }
+//
+//
+//     // does the system percolate?
+//     public boolean percolates() {
+//
+//     }
+//
+// }
