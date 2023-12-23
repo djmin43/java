6201949e6aa9560805afd6138d4973d3abb75800
dongjoon min <dj.min43@gmail.com>
2023-12-23 21:52:53 +0900

    permutation with list (easy way)

--- hello/Permuation.java
+++ hello/Permuation.java
@@ -4,8 +4,17 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Permuation {
     public static void main(String[] args) {
-
+        String testString = "A B C D E F G H I";
+        String string = StdIn.readString();
+        Integer k = Integer.parseInt(string);
+        String[] stringList = testString.split(" ");
+        for (int i = 0; i < k; i++) {
+            System.out.println(stringList[StdRandom.uniformInt(0, stringList.length)]);
+        }
     }
 }
