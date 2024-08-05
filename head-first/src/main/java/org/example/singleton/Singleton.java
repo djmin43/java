96892399b9a8893e201d849507bae8913429cf20
dongjoon min <dj.min43@gmail.com>
2024-08-05 19:45:15 +0900

    add singleton

---
+++ head-first/src/main/java/org/example/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package org.example.singleton;
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
