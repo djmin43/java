15d2ddeb65667841c9c8f0e93fb6b2410bb6e7da
dongjoon min <dj.min43@gmail.com>
2024-12-08 18:38:38 +0900

    first part done

--- advent-of-code-2024/src/main/java/org/example/day1/HistorianHysteria.java
+++ advent-of-code-2024/src/main/java/org/example/day1/HistorianHysteria.java
@@ -1,31 +1,40 @@
 package org.example.day1;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class HistorianHysteria {
 
     public static void main(String[] args) {
-        System.out.println("history hiysteria");
         try {
-            String path = System.getProperty("user.dir")+ "/src/main/resources";
+            String path = System.getProperty("user.dir") + "/src/main/resources";
+
+            Integer sum = 0;
+            ArrayList<Integer> firstList = new ArrayList<>();
+            ArrayList<Integer> secondList = new ArrayList<>();
 
             File myFile = new File(path + "/location-id.txt");
             Scanner myReader = new Scanner(myFile);
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+                String[] data = myReader.nextLine().split(" ", 4);
+                firstList.add(Integer.parseInt(data[0]));
+                secondList.add(Integer.parseInt(data[3]));
             }
             myReader.close();
+
+            Collections.sort(firstList);
+            Collections.sort(secondList);
+            for (int i = 0; i < firstList.size(); i++) {
+                sum += Math.abs(firstList.get(i) - secondList.get(i));
+            }
+            System.out.println(sum);
+
         } catch (FileNotFoundException e) {
             System.out.println("file not found");
         }
-
-
-
 
 
     }
