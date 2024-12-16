04cb643f2c057e268da595aead035e4da3be9526
dongjoon min <dj.min43@gmail.com>
2024-12-16 21:46:49 +0900

    day 2 part2 wip

--- advent-of-code-2024/src/main/java/org/example/day2/RedNosedReports.java
+++ advent-of-code-2024/src/main/java/org/example/day2/RedNosedReports.java
@@ -4,7 +4,6 @@ import org.example.util.TxtReader;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class RedNosedReports {
@@ -14,7 +13,10 @@ public class RedNosedReports {
         Scanner codes = TxtReader.scanner("red-nosed-reports.txt");
         List<ArrayList<Integer>> parsedRedNosedReports = RedNosedReports.parseRedNosedReports(codes);
         Integer partOne = RedNosedReports.partOne(parsedRedNosedReports);
+        Integer partTwo = RedNosedReports.partTwo(parsedRedNosedReports);
+
         System.out.println(partOne);
+        System.out.println(partTwo);
     }
 
 
@@ -22,39 +24,74 @@ public class RedNosedReports {
         Integer safeReports = 0;
 
         for (int i = 0; i < reports.size(); i++) {
-            boolean isSafe = true;
-            Integer firstCode = reports.get(i).get(0);
-            Integer lastCode = reports.get(i).get(reports.get(i).size() - 1);
-            boolean isAscending = firstCode < lastCode;
-
-
-            for (int j = 0; j < reports.get(i).size() - 1; j++) {
-                Integer currentCode = reports.get(i).get(j);
-                Integer nextCode = reports.get(i).get(j + 1);
-
-                if (isAscending) {
-                    if (currentCode >= nextCode) {
-                        isSafe = false;
-                    }
-                    if (Math.abs(currentCode - nextCode) > 3) {
-                        isSafe = false;
-                    }
-                } else {
-                    if (currentCode <= nextCode) {
-                        isSafe = false;
-                    }
-                    if (Math.abs(currentCode - nextCode) > 3) {
-                        isSafe = false;
-                    }
-                }
-
-            }
+            boolean isSafe = validator(reports.get(i)) == -1;
 
             if (isSafe) {
                 safeReports++;
             }
         }
         return safeReports;
+
+    }
+
+    public static Integer partTwo(List<ArrayList<Integer>> reports) {
+        Integer safeReports = 0;
+        for (int i = 0; i < reports.size(); i++) {
+            boolean isSafe = true;
+            int issueLayerIndex = validator(reports.get(i));
+
+            if (issueLayerIndex != -1) {
+                ArrayList<Integer> temp = reports.get(i);
+                temp.remove(validator(temp));
+                if (validator(temp) == -1) {
+                    isSafe = true;
+                } else {
+                    isSafe = false;
+                }
+            }
+
+            if (isSafe) {
+                safeReports++;
+
+            }
+        }
+        return safeReports;
+
+    }
+
+    public static int validator(ArrayList<Integer> currentReport) {
+
+        int unsafeIndex = -1;
+        Integer firstCode = currentReport.get(0);
+        Integer lastCode = currentReport.get(currentReport.size() - 1);
+        boolean isAscending = firstCode < lastCode;
+
+        for (int j = 0; j < currentReport.size() - 1; j++) {
+            Integer currentCode = currentReport.get(j);
+            Integer nextCode = currentReport.get(j + 1);
+
+            if (isAscending) {
+                if (currentCode >= nextCode) {
+                    unsafeIndex = j + 1;
+                    break;
+                }
+                if (Math.abs(currentCode - nextCode) > 3) {
+                    unsafeIndex = j + 1;
+                    break;
+                }
+            } else {
+                if (currentCode <= nextCode) {
+                    unsafeIndex = j + 1;
+                    break;
+                }
+                if (Math.abs(currentCode - nextCode) > 3) {
+                    unsafeIndex = j + 1;
+                    break;
+                }
+            }
+        }
+
+        return unsafeIndex;
 
     }
 
