3b9bfe3bc911b53142de7919e807674f52e01cc4
dongjoon min <dj.min43@gmail.com>
2024-12-24 19:08:28 +0900

    done

--- younghan-java/src/main/java/org/example/scanner/Scanner1.java
+++ younghan-java/src/main/java/org/example/scanner/Scanner1.java
@@ -7,19 +7,19 @@ public class Scanner1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("사용자가 입력한 정수를 계산해주는 프로그램. 0을 입력하면 꺼짐");
+        int sum = 0;
         while (true) {
-            System.out.println("첫번째 숫자를 입력해주세요: ");
-            int firstInt = scanner.nextInt();
+            System.out.println("숫자를 입력해보세요!");
 
-            System.out.println("두번째 숫자를 입력해주세요:");
-            int secondInt = scanner.nextInt();
+            int input = scanner.nextInt();
 
-            if ((firstInt + secondInt) == 0) {
-                System.out.println("두 합은 0입니다. 프로그램 종료");
+            if (input == 0) {
+                System.out.println("프로그램 종료");
                 break;
             }
-            System.out.println("두 수의 합은? " + (firstInt + secondInt));
-
+            sum += input;
+            System.out.println("합계는요~ " + sum);
 
         }
 
