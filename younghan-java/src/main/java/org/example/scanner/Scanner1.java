2f04e584633a865e1f1c9cf7df9def3c207e7982
dongjoon min <dj.min43@gmail.com>
2024-12-24 17:57:48 +0900

    done

--- younghan-java/src/main/java/org/example/scanner/Scanner1.java
+++ younghan-java/src/main/java/org/example/scanner/Scanner1.java
@@ -7,17 +7,17 @@ public class Scanner1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("문자열을 입력하세요:");
-        String str = scanner.nextLine(); // 입력을 String으로 가져온다.
-        System.out.println("임력한 문자열: " + str);
+        System.out.println("문자열을 입력하세요(exit: 종료):");
 
-        System.out.print("정수를 입력하세요: ");
-        int intValue = scanner.nextInt();
-        System.out.println("임력한 정수: " + intValue);
+        while (true){
+            String str = scanner.nextLine();
 
+            if (str.equals("exit")) {
+                System.out.print("프로그램을 종료합니다");
+                break;
+            }
+            System.out.println("입력한 문자열: " + str);
+        }
 
-        System.out.print("실수를 입력하세요: ");
-        double doubleValue = scanner.nextDouble();
-        System.out.println("임력한 실수: " + doubleValue);
     }
 }
