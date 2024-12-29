4f078b8765f9d13a3759d37216251777c512d5bf
dongjoon min <dj.min43@gmail.com>
2024-12-29 16:37:37 +0900

    refactor

--- younghan-java/src/main/java/org/example/memory/ex/MathArrayUtilsMain.java
+++ younghan-java/src/main/java/org/example/memory/ex/MathArrayUtilsMain.java
@@ -1,13 +1,15 @@
 package org.example.memory.ex;
 
+import static org.example.memory.ex.MathArrayUtils.*;
+
 public class MathArrayUtilsMain {
 
     public static void main(String[] args) {
         int[] values = {1, 2, 3, 4, 5};
 
-        System.out.println("sum = " + MathArrayUtils.sum(values));
-        System.out.println("sum = " + MathArrayUtils.average(values));
-        System.out.println("sum = " + MathArrayUtils.min(values));
-        System.out.println("sum = " + MathArrayUtils.max(values));
+        System.out.println("sum = " + sum(values));
+        System.out.println("average = " + average(values));
+        System.out.println("min = " + min(values));
+        System.out.println("max = " + max(values));
     }
 }
